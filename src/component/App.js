519b69c665292ea80c6ad7c1a558fe962b8d29ef
pado8 <floe9235@gmail.com>
2023-08-11 17:18:52 +0900

    CSS 리셋 추가

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Main from './component/Main';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 
 
--- src/component/Main.js
+++ src/component/Main.js
@@ -4,6 +4,7 @@ function Main(props) {
     return (
         <div className='main'>
             <h1>메인 부분 입니다</h1>
+            <h2>프론트엔드 개발자 노태효의 포트폴리오 사이트</h2>
         </div>
     );
 }
